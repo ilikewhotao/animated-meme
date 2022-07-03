1d35cb603965315acb9430ce698431c9fee83477
ilikewhotao <ilikewhotao@qq.com>
2022-07-03 18:15:26 +0800

    2022-07-03

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-function goSubmit() {
-  console.log(1);
+function goSubmit(e) {
+  const formData = new FormData(e.target);
+  const username = formData.get("username");
+  console.log(username);
+  const data = {};
+  for (const item of formData) {
+    data[item[0]] = item[1];
+  }
+  console.log(data);
   return false;
 }
